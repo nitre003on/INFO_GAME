bc9442aafb72b43dee0ee6cac5a2f8b121e8cc76
RustingRobot <rusting.robot.scratch@gmail.com>
2020-05-14 20:57:42 +0200

    Schuss in Richtung des Mauses

---
+++ MouseInput.java
@@ -0,0 +1,19 @@
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MouseInput extends MouseAdapter {
+	
+	public boolean rightClick;
+
+	private Handler handler;
+	
+	public MouseInput(Handler handler) {
+		this.handler = handler;
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		handler.addObject(new DirectionalShot(Game.player,new Vector2(e.getX(),e.getY()),ID.Shot,handler));
+		System.out.println("shoooooot");
+	}
+}
