e868140136498502fd20eb6abdc266a2d555ed32
Hier0nymus <karl@your-memory.de>
2020-05-14 22:05:44 +0200

    Wall collision hinzugefügt

--- Player.java
+++ Player.java
@@ -13,21 +13,57 @@ import java.util.Scanner;
 public class Player extends GameObject {
   
   Direction direction;
+  Rectangle hitBox;
   
   public Player(int x, int y, ID id, Handler handler, Direction direction) {
     super(x, y, id, handler);
     this.direction = direction;
+    hitBox = new Rectangle(x, y, 32, 32);
   }
   
   public Rectangle getBounds() {
     return new Rectangle((int)x,(int)y,32,32);                                            //Methode um die Umrisse zu kriegen
   }
-
+  
   public void tick() {
+    hitBox.x += velX;
+    for (int i = 0;i < handler.objects.size();i++) {        
+      GameObject tempObject = handler.objects.get(i);
+      if(handler.objects.get(i).getID() == ID.Wall){
+        if (hitBox.intersects(tempObject.getBounds())){
+          hitBox.x -= velX;
+          while (!hitBox.intersects(tempObject.getBounds())){
+            hitBox.x += Math.signum(velX);
+          }
+          hitBox.x -= Math.signum(velX);
+          velX = 0;
+          x = hitBox.x;
+        }
+      }
+    }
+    
+    hitBox.y += velY;
+    for (int i = 0;i < handler.objects.size();i++) {        
+      GameObject tempObject = handler.objects.get(i);
+      if(handler.objects.get(i).getID() == ID.Wall){
+        if (hitBox.intersects(tempObject.getBounds())){
+          hitBox.y -= velY;
+          while (!hitBox.intersects(tempObject.getBounds())){
+            hitBox.y += Math.signum(velY);
+          }
+          hitBox.y -= Math.signum(velY);
+          velY = 0;
+          y = hitBox.y;
+        }
+      }
+    }
+    
     x+=velX;                                                          //Bewegungsrichtumg
     y+=velY;                                                          
     x=Game.clamp(x, 0, Game.WIDTH-37);                                              //das innerhalb des Fensters bleiben
     y=Game.clamp(y, 0, Game.HEIGHT-64);
+    hitBox.x = (int)x;
+    hitBox.y = (int)y; 
     handler.addObject(new BasicTrail((int)x, (int)y, ID.Trail, Color.white, 32, 32, 0.08f, handler));             //"Schwanz" ran h�ngen
     collision();                                                        //Kollisionsmethode
   }
@@ -50,17 +86,17 @@ public class Player extends GameObject {
       }
     }
   }
-  
+      
   public void shoot() {
     handler.addObject(new Shot((int) x,(int) y, direction, ID.Shot, handler));                                //Schuss methode(ein Schuss Object wird erstellt
   } 
-  
+      
   public void interact() { 
-      x = x*(-1);
-      y = y*(-1);
+    x = x*(-1);
+    y = y*(-1);
   }
-
-  
+      
+      
   public void render(Graphics g) {
     
     /*Graphics2D g2d = (Graphics2D) g;
@@ -70,6 +106,6 @@ public class Player extends GameObject {
     g.fillRect((int)x, (int)y, 32, 32);                                                   // Form wird ge"zeichnet"
     
   }
-  
-  
+      
+      
 }
